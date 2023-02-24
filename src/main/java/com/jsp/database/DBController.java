06c3965b23de024f26c62fd0232530bcb440fac2
ChangDaeJun <eowns1209@gmail.com>
2023-02-24 16:27:39 +0900

    refactor : DB.java -> DBController 로 변경

--- src/main/java/com/jsp/database/DBController.java
+++ src/main/java/com/jsp/database/DBController.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-public class DB {
+public class DBController {
 
     private static void execute(String query, String... args){
         Connection conn = null;
