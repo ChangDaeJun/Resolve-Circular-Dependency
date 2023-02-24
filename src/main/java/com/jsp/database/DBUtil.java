79d0289a409a2cca06330a478ca1d194e2897cb7
ChangDaeJun <eowns1209@gmail.com>
2023-02-24 14:23:17 +0900

    creat : DBUtil

---
+++ src/main/java/com/jsp/database/DBUtil.java
@@ -0,0 +1,54 @@
+package com.jsp.database;
+
+import org.h2.Driver;
+
+import java.sql.*;
+
+public class DBUtil {
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            DriverManager.registerDriver(new Driver());
+
+            String jdbcURL = "jdbc:h2:./board";
+            conn = DriverManager.getConnection(jdbcURL, "dbconnect", "1234");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void close(PreparedStatement stmt, Connection comm) {
+        try {
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            comm.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(ResultSet rs, PreparedStatement stmt, Connection comm) {
+        try {
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            comm.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
