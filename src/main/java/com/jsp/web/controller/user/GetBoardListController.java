bb0886a8a443a3812dfeac427dbd6141b362fe44
ChangDaeJun <eowns1209@gmail.com>
2023-02-26 12:01:18 +0900

    update : getBoardList에 작성자 추가

--- src/main/java/com/jsp/web/controller/user/GetBoardListController.java
+++ src/main/java/com/jsp/web/controller/user/GetBoardListController.java
@@ -2,6 +2,8 @@ package com.jsp.web.controller.user;
 
 import com.jsp.biz.board.BoardDAO;
 import com.jsp.biz.board.BoardVO;
+import com.jsp.biz.user.UserDAO;
+import com.jsp.biz.user.UserVO;
 import com.jsp.web.controller.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,6 +16,17 @@ public class GetBoardListController implements Controller {
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
             BoardDAO boardDAO = new BoardDAO();
             List<BoardVO> boardVOList = boardDAO.getList();
+
+            for(BoardVO board : boardVOList){
+                UserVO userVO = new UserVO();
+                userVO.setId(board.getUserId());
+
+                UserDAO userDAO = new UserDAO();
+                UserVO createUser = userDAO.findById(userVO);
+
+                board.setUserName(createUser.getName());
+            }
+
             request.setAttribute("boardList", boardVOList);
 
             return "GetBoardList";
