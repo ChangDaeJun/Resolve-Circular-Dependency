0591ad17cb4220775f1e206abb9effad70254184
ChangDaeJun <eowns1209@gmail.com>
2023-03-07 02:49:27 +0900

    refactor : 댓글 생성 서비스 제작

--- src/main/java/com/jsp/web/controller/comment/InsertCommentController.java
+++ src/main/java/com/jsp/web/controller/comment/InsertCommentController.java
@@ -4,6 +4,7 @@ import com.jsp.biz.comment.CommentDAO;
 import com.jsp.biz.comment.CommentVO;
 import com.jsp.biz.user.UserVO;
 import com.jsp.web.controller.Controller;
+import com.jsp.web.service.comment.InsertCommentService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.h2.engine.User;
@@ -11,17 +12,22 @@ import org.h2.engine.User;
 public class InsertCommentController implements Controller {
     @Override
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+        String boardId = request.getParameter("id");
+
+        CommentVO insertCommentVO = getInsertCommentVO(request);
+        InsertCommentService.getInstance().run(insertCommentVO);
+
+        return "getBoard.do?id" + boardId;
+    }
+
+    private static CommentVO getInsertCommentVO(HttpServletRequest request) {
         UserVO createUSer = (UserVO) request.getSession().getAttribute("user");
         String text = request.getParameter("text");
         String boardId = request.getParameter("id");
-
         CommentVO commentVO = new CommentVO();
         commentVO.setUserId(createUSer.getId());
         commentVO.setBoardId(Long.parseLong(boardId));
         commentVO.setText(text);
-        CommentDAO commentDAO = new CommentDAO();
-        commentDAO.insert(commentVO);
-
-        return "getBoard.do?id" + boardId;
+        return commentVO;
     }
 }
